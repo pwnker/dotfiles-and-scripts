ef3e30e6e0982cdb6a173e4071088a236a72fbdd
Pwnker <contact@pwnker.com>
2021-08-14 12:01:44 +0100

    Update launch_env.sh

--- launch_env.sh
+++ launch_env.sh
@@ -5,11 +5,11 @@ apt install zsh
 chsh -s $(which zsh)
 
 # clone config files
-wget -O ~/.zshrc https://raw.githubusercontent.com/pwnker/dotfiles-and-scripts/main/.zshrc
-wget -O ~/.tmux.conf https://raw.githubusercontent.com/pwnker/dotfiles-and-scripts/main/.tmux.conf
+wget -O $HOME/.zshrc https://raw.githubusercontent.com/pwnker/dotfiles-and-scripts/main/.zshrc
+wget -O $HOME/.tmux.conf https://raw.githubusercontent.com/pwnker/dotfiles-and-scripts/main/.tmux.conf
 
 mkdir ~/.config/nvim/
-wget -O ~/.config/nvim/init.vim https://raw.githubusercontent.com/pwnker/dotfiles-and-scripts/main/init.vim
+wget -O $HOME/.config/nvim/init.vim https://raw.githubusercontent.com/pwnker/dotfiles-and-scripts/main/init.vim
 
 # install tmux
 apt install tmux
